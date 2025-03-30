df2c2c66d86d856fa3db2c822384880fb594696d
agshruti12 <agshruti2901@gmail.com>
2025-03-30 04:10:43 -0400

    content script changes

--- src/content.js
+++ src/content.js
@@ -1,13 +1,14 @@
 // src/content.js
 
-const allContent = document.getElementsByTagName("html");
-let siteText = "";
-for (const tag of allContent) {
-  siteText += "\n" + tag.innerText;
-}
-
 const port = chrome.runtime.connect({ name: "sendSiteText" });
 port.onMessage.addListener(function(msg) {
   if (msg.flag)
+
+    var allContent = document.getElementsByTagName("html");
+    let siteText = "";
+    for (const tag of allContent) {
+        siteText += "\n" + tag.innerText;
+    };
+
     port.postMessage({ siteText: siteText });
 });
